f55c7c45536541bd0c25ecb74b86487197e20e9d
Ivanio1 <sobolev1van@yandex.ru>
2023-06-14 16:16:42 +0300

    initializing new project

---
+++ src/test/java/com/ivanio/swaphub/SwapHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ivanio.swaphub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwapHubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
