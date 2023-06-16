cfde33f7f66a926f4a061cb6e5f9dcfd762d3746
Ivanio1 <sobolev1van@yandex.ru>
2023-06-16 16:19:03 +0300

    DataBase access

--- src/main/java/com/ivanio/swaphub/controllers/ProductController.java
+++ src/main/java/com/ivanio/swaphub/controllers/ProductController.java
@@ -1,21 +1,23 @@
 package com.ivanio.swaphub.controllers;
 
 import com.ivanio.swaphub.models.Product;
+import com.ivanio.swaphub.services.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequiredArgsConstructor
-public class productController {
-    private final com.ivanio.swaphub.services.productService productService;
+public class ProductController {
+    private final ProductService productService;
 
     @GetMapping("/")
-    public String products(Model model){
-        model.addAttribute("products",productService.getProducts());
+    public String products(@RequestParam(name= "title", required = false) String title, Model model){
+        model.addAttribute("products",productService.getProducts(title));
         return "products";
     }
 
