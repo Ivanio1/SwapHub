cec381df99541f58ed42be84b8c200c17de287a9
Ivanio1 <sobolev1van@yandex.ru>
2023-10-18 17:27:16 +0300

    add eng

--- src/main/java/com/ivanio/swaphub/controllers/ProductController.java
+++ src/main/java/com/ivanio/swaphub/controllers/ProductController.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
-import java.nio.channels.MulticastChannel;
 import java.security.Principal;
 
 @Controller
